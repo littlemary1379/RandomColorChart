dc79d0cf3aad25ccbac687b698f25a948c0f3870
Mary <littlemary1379@gmail.com>
2021-03-12 10:11:48 +0900

    create camera preview

--- app/src/main/java/com/mary/kotlinprojectstudy/camera/CameraActivity.kt
+++ app/src/main/java/com/mary/kotlinprojectstudy/camera/CameraActivity.kt
@@ -4,11 +4,17 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 import com.mary.kotlinprojectstudy.R
 import com.mary.kotlinprojectstudy.util.DlogUtil
 import com.mary.kotlinprojectstudy.util.PermissionUtil
 import java.io.File
+import java.lang.Exception
 import java.util.concurrent.ExecutorService
 
 class CameraActivity : AppCompatActivity() {
@@ -16,6 +22,8 @@ class CameraActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "CameraActivity"
     }
+
+    private lateinit var previewView : PreviewView
 
     private lateinit var imageCapture: ImageCapture
 
@@ -26,6 +34,7 @@ class CameraActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
+        findView()
         permissionCheck()
     }
 
@@ -35,6 +44,8 @@ class CameraActivity : AppCompatActivity() {
 
         if(!PermissionUtil.checkPermission(this, permissionList)) {
             PermissionUtil.requestPermission(this, permissionList)
+        } else {
+            openCamera()
         }
     }
 
@@ -47,10 +58,41 @@ class CameraActivity : AppCompatActivity() {
 
         if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED ) {
             DlogUtil.d(TAG, "승인")
+            openCamera()
         } else {
             DlogUtil.d(TAG, "승인 거부")
             onBackPressed()
         }
+    }
+
+    private fun findView(){
+        previewView = findViewById(R.id.previewView)
+    }
+
+    private fun openCamera(){
+        DlogUtil.d(TAG, "openCamera")
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider : ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e : Exception) {
+                DlogUtil.d(TAG, "바인딩 실패 $e")
+            }
+        }, ContextCompat.getMainExecutor(this))
+
     }
 
 }
